51b085bce7808436cb1f4f6ec6f80b5aff370321
Lucas Bent <lucasowenbent@gmail.com>
2025-01-05 17:39:04 -0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.tsx'
-import { HashRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import Demands from './Demands.tsx'
 import Minutes1982 from './Minutes1982.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/demands' element={<Demands />} />
                 <Route path='/minutes-1982' element={<Minutes1982 />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     </StrictMode>
 )
