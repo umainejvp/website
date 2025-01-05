b88245f037fb835068510535b073f172298e9758
Lucas Bent <lucasowenbent@gmail.com>
2025-01-05 17:02:40 -0500

    try HashRouter again

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router'
 import Demands from './Demands.tsx'
 import Minutes1982 from './Minutes1982.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/demands' element={<Demands />} />
                 <Route path='/minutes-1982' element={<Minutes1982 />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </StrictMode>
 )
